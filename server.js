200070691d0ca23f5f08ddb7909a9439204eadbb
BCADOMAIN\U549179 <U549179@bca.co.id>
2023-05-04 08:30:08 +0700

    Env adjustment again

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const Log = require('./app/utils/Logging')
+const jsonMessage = require('./app/jsonFormat/jsonMessage')
 require('dotenv').config()
 
 const corsOptions = { origin: '*' }
@@ -18,6 +19,10 @@ require('./app/routes/index')(app)
 
 app.use('/api/health-check', (req, res) => {
     res.status(200).send('Welcome to MudaMude')
+})
+
+app.use('*', (req, res) => {
+    res.status(404).send(jsonMessage.jsonFailed(404, `Route ${req.method} ${req.baseUrl} not found`))
 })
 
 const PORT = process.env.PORT || 4000
